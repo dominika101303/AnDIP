9b200244383dc8ecfdaee6334c24468019305a2d
dominika <dominikakledzik@gmail.com>
2014-04-09 22:46:54 +0200

    poprawki

--- week2/andip/src/andip/arangodb.py
+++ week2/andip/src/andip/arangodb.py
@@ -4,9 +4,11 @@ Created on 15 mar 2014
 
 @author: dominika
 '''
-import sys
-from arango import create, c
-from andip.default import DefaultProvider 
+
+from arango import create
+from andip.default import DefaultProvider
+
+
 class ArangoProvider(DefaultProvider):
     def __init__(self, backoff=None):
         """Provider which browse data in neo4j graph database.
@@ -20,7 +22,7 @@ class ArangoProvider(DefaultProvider):
         self.__nodeCounter = 0
         self.conn = None
         self.connect()
-        
+
     def __isLeaf(self, node):
         """Method which verifies if node is a leaf.
 
@@ -30,11 +32,11 @@ class ArangoProvider(DefaultProvider):
         if len(self.getChildren(node)):
             return False
         return True
-    
+
     def __getNodeConf(self, origin):
         """Method which gets configuration for given word.
 
-        :param origin: checked node 
+        :param origin: checked node
         :type origin: tuple
         """
         node = origin
@@ -61,7 +63,7 @@ class ArangoProvider(DefaultProvider):
         lemma = resultDict["word"]
         del resultDict["word"]
         return (pos, lemma, resultDict)
-    
+
     def _get_conf(self, word):
         nodes = self.getNodes(word)
         if len(nodes) == 0:
@@ -71,9 +73,10 @@ class ArangoProvider(DefaultProvider):
             raise LookupError("word=%s" % word)
         result = [self.__getNodeConf(leaf) for leaf in leaves]
         return result
-    
+
     def __getLeaf(self, node, criteria):
-        """Method which gets leaf for given criteria and in subtree in which the root is given node.
+        """Method which gets leaf for given criteria and in subtree in which
+        the root is given node.
 
         :param node: subtree root
         :type node: tuple
@@ -89,9 +92,10 @@ class ArangoProvider(DefaultProvider):
                         del criteria[nodeChild.body["name"]]
                         return self.__getLeaf(child, criteria)
         return None
-    
+
     def __getWordNode(self, posNode, word):
-        """Method which gets subtree root for given part of speech and canonical form of word.
+        """Method which gets subtree root for given part of speech and
+        canonical form of word.
 
         :param posNode: root which represents part of speech
         :type posNode: tuple
@@ -104,7 +108,7 @@ class ArangoProvider(DefaultProvider):
                     if node.body["name"] == word:
                         return node
         return None
-    
+
     def _get_word(self, conf):
         pos = conf[0]
         word = conf[1]
@@ -119,18 +123,18 @@ class ArangoProvider(DefaultProvider):
         if wordNode is None:
             raise LookupError("conf=%s" % str(conf))
         return self.__getLeaf(wordNode, criteria)
-    
+
     def connect(self):
         """Method which creates connection with database.
         """
         self.conn = create(db="test")
         self.conn.database.create()
-    
+
     def close(self):
         """Method which deletes handler to connection with database.
         """
         self.conn = None
-    
+
     def node(self, id, data):
         """Method which adds node to graph.
 
@@ -139,21 +143,23 @@ class ArangoProvider(DefaultProvider):
         :param data: name of node
         :type data: str
         """
-        node = self.conn.test.documents.create({"name":data})
+        node = self.conn.test.documents.create({"name": data})
         self.nodes.append(node)
-    
+
     def getNodes(self, criteria):
         """Method which gets node whose name is given in criteria
 
         :param criteria: name of node
         :type criteria: str
         """
-        
+
         result = []
-        for elem in self.conn.connection.query(self.conn.test.query.filter("obj.name == '%s'"%criteria).build_query()):
+        for elem in self.conn.connection.query(
+            self.conn.test.query.filter("obj.name == '%s'" % criteria)
+            .build_query()):
             result.append(elem)
-        return result;
-            
+        return result
+
     def rel(self, startNode, endNode, data):
         """Method which creates relation between given nodes.
 
@@ -164,24 +170,23 @@ class ArangoProvider(DefaultProvider):
         :param data: name of relation
         :type data: string
         """
-        rel = self.conn.test_edges.edges.create(self.nodes[startNode], self.nodes[endNode], {"label": data})
+        rel = self.conn.test_edges.edges.create(self.nodes[startNode],
+        self.nodes[endNode], {"label": data})
         self.rels.append(rel)
-    
+
     def graph(self):
         """Method which initialize empty graph
         """
         self.nodes = []
         self.rels = []
 
-        
     def dropAll(self):
         """Method which deletes all nodes and relations from database
         """
         self.conn.test.delete()
         self.conn.test_edges.delete()
         self.connect()
-        
-        
+
     def getParent(self, node):
         """Method which gets node which is parent of given node.
 
@@ -192,10 +197,11 @@ class ArangoProvider(DefaultProvider):
         result = []
         for edge in self.conn.connection.query(query):
             for node in self.conn.connection.query(
-            self.conn.test.query.filter("obj._id == '%s'" % edge["_to"]).build_query()):
+            self.conn.test.query.filter("obj._id == '%s'" % edge["_to"])
+            .build_query()):
                 result.append(node)
-        return result;
-        
+        return result
+
     def getChildren(self, node):
         """Method which gets all children of given node.
 
@@ -206,10 +212,11 @@ class ArangoProvider(DefaultProvider):
         result = []
         for edge in self.conn.connection.query(query):
             for node in self.conn.connection.query(
-            self.conn.test.query.filter("obj._id == '%s'" % edge["_to"]).build_query()):
+            self.conn.test.query.filter("obj._id == '%s'" % edge["_to"])
+            .build_query()):
                 result.append(node)
-        return result;
-    
+        return result
+
     def _importDict(self, name, dataDict):
         """Method which imports tree for given part of speech.
 
@@ -236,13 +243,13 @@ class ArangoProvider(DefaultProvider):
             self.rel(nodeId, childId, "CHILD")
             self.rel(childId, nodeId, "PARENT")
         return nodeId
-    
+
     def importData(self, dataDict):
-        """Methods which imports data into database. 
+        """Methods which imports data into database.
 
         :param dataDict: data dictionary (the same as for fileProvider)
         :type dataDict: dict
-        
+
         """
         self.conn.test.create()
         self.conn.test_edges.create_edges()
